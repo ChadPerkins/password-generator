cd26ca3cf1b5bf08afad7f215d5265ea7301fa1f
ChadPerkins <web3.engineering@gmail.com>
2022-04-02 16:23:09 -0400

    Added error handling and optional website input

--- generate.py
+++ generate.py
@@ -4,19 +4,40 @@ from pathlib import Path
 pwdNums = string.ascii_lowercase + string.ascii_uppercase + \
     string.digits + "@%+\\/'!#$^?:,(){}[]~"
 
-username = input("Enter your username associated with the password: ")
-length = int(input('Enter the password length: '))
+while True:
+    username = input("Enter your username associated with the password: ")
+    if len(username) < 1:
+        print("A username must be associated with the password.")
+        continue
+    else:
+        break
 
+website = input("Enter associated website (optional): ")
 
-temp = random.sample(pwdNums, length)
+while True:
+    length = input('Enter the password length: ')
+    if len(length) < 1:
+        print("Password length must be greater then 0.")
+        continue
+    else:
+        break
+
+temp = random.sample(pwdNums, int(length))
 password = "".join(temp)
 print(password)
 
 user = username + ": " + password
+userLog = f"{username}@{website}: {password}"
 
 if Path('generatedpwds.txt').is_file():
     with open('generatedpwds.txt', mode='a') as file:
-        file.write(user + "\n")
+        if len(website) > 0:
+            file.write(userLog + "\n")
+        else:
+            file.write(user + "\n")
 else:
     with open('generatedpwds.txt', mode='w') as file:
-        file.write(user + "\n")
+        if len(website) > 0:
+            file.write(userLog + "\n")
+        else:
+            file.write(user + "\n")
